1f788baa07845aadcdfc85b032a7b7de9e772f88
Roberto M. Oliva <floyd303@gmail.com>
2015-10-12 15:52:31 +0200

    First refactorization: Separation of streams and properties

--- app/assets/javascripts/admin.js/src/modules/users/model.js
+++ app/assets/javascripts/admin.js/src/modules/users/model.js
@@ -1,158 +1,197 @@
 NS('AdminJS.modules.users');
 
-AdminJS.modules.users.FSM = function(sb) {
+AdminJS.modules.users.FSM = function(sb, input_stream) {
 	var stream_dispatcher = sb.streams.signal(sb);
 	
     var ockham_fsm = Ockham.create({
-    config: function(fsm) {
-      return {
-        states: {
-          none: {
-            init: "index"
-          },
-          index: {
-            page: 'index', // this.pageTransition,
-            show: "show_form",
-            new: 'new_form',
-            create: 'index',
-            edit: 'edit_form',
-            update: 'index',
-            delete: 'delete_form',
-            destroy: 'index'
-          },
-          show_form: {
-            states: {
-              edit_form: {
-                cancel: 'show_form',
-                update: 'index',
-                delete: 'show_form-edit_form-delete_form',
-                states: {
-                  delete_form: {
-                    cancel: 'show_form-edit_form',
-                  }
-                }
-              },
-              delete_form: {
-                cancel: 'show_form',
-              }
-            },
-            edit: 'show_form-edit_form',
-            delete: 'show_form-delete_form',
-            cancel: 'index',
-            destroy: 'index'
-          },
-          new_form: {
-            create: 'index',
-            cancel: 'index'
-          },
-          edit_form: {
-            states: {
-              delete_form: {
-                cancel: 'edit_form',
-                destroy: 'index'
-              }
-            },
-            cancel: 'index',
-            update: 'index',
-            delete: 'edit_form-delete_form'
-          },
-          delete_form: {
-            destroy: 'index',
-            cancel: 'index'
-          }
-        }
-      };
-    }
-  });
+	    config: function(fsm) {
+	      return {
+	        states: {
+	          none: {
+	            init: "index"
+	          },
+	          index: {
+	            page: 'index', // this.pageTransition,
+	            show: "show_form",
+	            new: 'new_form',
+	            create: 'index',
+	            edit: 'edit_form',
+	            update: 'index',
+	            delete: 'delete_form',
+	            destroy: 'index'
+	          },
+	          show_form: {
+	            states: {
+	              edit_form: {
+	                cancel: 'show_form',
+	                update: 'index',
+	                delete: 'show_form-edit_form-delete_form',
+	                states: {
+	                  delete_form: {
+	                    cancel: 'show_form-edit_form',
+	                  }
+	                }
+	              },
+	              delete_form: {
+	                cancel: 'show_form',
+	              }
+	            },
+	            edit: 'show_form-edit_form',
+	            delete: 'show_form-delete_form',
+	            cancel: 'index',
+	            destroy: 'index'
+	          },
+	          new_form: {
+	            create: 'index',
+	            cancel: 'index'
+	          },
+	          edit_form: {
+	            states: {
+	              delete_form: {
+	                cancel: 'edit_form',
+	                destroy: 'index'
+	              }
+	            },
+	            cancel: 'index',
+	            update: 'index',
+	            delete: 'edit_form-delete_form'
+	          },
+	          delete_form: {
+	            destroy: 'index',
+	            cancel: 'index'
+	          }
+	        }
+	      };
+	    }
+    });
 
     var do_transition = function(transition, options) {
-      return ockham_fsm.doTransition(transition, options).then(function(ret) {
-    	  stream_dispatcher.dispatch(ret);
+      return ockham_fsm.doTransition(transition, options).then(function(event) {
+    	  stream_dispatcher.dispatch(event);
       });
     };
+
+    input_stream.onValue(function(options) {
+    	do_transition(options.action, options.options);
+    });
 	
+    var initialize = function() {
+    	return do_transition("init");
+    };
+    
 	return {
-		do_transition: do_transition,
+		initialize: initialize,
 		stream: stream_dispatcher.stream
 	};
 };
 
+AdminJS.modules.users.Streams = function(sb, input_streams) {
+	var fsm = new AdminJS.modules.users.FSM(sb, input_streams.jsx_stream);
+
+    var showSpinner = function() {
+    	spinner_stream.plug(Kefir.constant(true));
+    };
+    
+    var hideSpinner = function() {
+    	spinner_stream.plug(Kefir.constant(false));
+    };
+    
+	var index_stream = fsm.stream.filter(function(event) {
+      return event.to === 'index';
+    });
+	
+	var show_id_stream = input_streams.jsx_stream.filter(function(event) {
+		return "show" === event.action;
+	}).map(function(event) {
+		return event.options.row.id;
+	});
+	
+	var get_show_stream = show_id_stream.flatMapLatest(function(id) {
+		showSpinner();
+		return Kefir.fromPromise(sb.services.get(
+				"users", 'show', {id: id}
+			).then(function(data) {
+				hideSpinner();
+				return data;
+			})
+		);
+	});
+
+	var selected_stream = get_show_stream.map(function(data) {
+	    return data.data;
+    });
+	
+	// El page se obtiene de las opciones de lanzar la transición
+	// "page" a la máquina de estados
+	var page_stream = fsm.stream.filter(function(event) {
+      return event.to === 'index';
+    }).map(function(fsm_event) {
+		if(fsm_event.options && fsm_event.options.page) {
+			return fsm_event.options.page;
+		} else {
+			return 1;
+		}
+    });
+
+	var get_index_stream = page_stream.flatMapLatest(function(page) {
+		showSpinner();
+		return Kefir.fromPromise(sb.services.get(
+				"users", 'index', {page: page, per_page: 10}
+			).then(function(data) {
+				hideSpinner();
+				return data;
+			})
+		);
+	});
+	
+	var records_stream = get_index_stream.map(function(data) {
+	      return data.data;
+    });
+
+	var total_stream = get_index_stream.map(function(data) {
+	      return data.total;
+    });
+
+    var spinner_stream = Kefir.pool();
+    
+    var initialize = function() {
+    	return fsm.initialize();
+    };
+
+    return {
+    	initialize: initialize,
+		fsm_stream: fsm.stream,
+		page_stream: page_stream,
+		selected_stream: selected_stream,
+		records_stream: records_stream,
+		total_stream: total_stream,
+		spinner_stream: spinner_stream
+	};
+};
 
 AdminJS.modules.users.Model = (function() {
   var creator = function(sb, dispatcher_stream) {
-	var fsm = new AdminJS.modules.users.FSM(sb);
+	    
+	var streams = new AdminJS.modules.users.Streams(sb, {
+		jsx_stream: dispatcher_stream
+	});
 	  
-    var stateProperty = fsm.stream;
+	// Properties
+    var stateProperty = streams.fsm_stream.toProperty();
+    stateProperty.log();
     var perPageProperty = Kefir.constant(10);
-    var spinnerStream = Kefir.pool();
-    var spinnerProperty = spinnerStream.toProperty();
-    
-    var showSpinner = function() {
-      spinnerStream.plug(Kefir.constant(true));
-    };
-    var hideSpinner = function() {
-      spinnerStream.plug(Kefir.constant(false));
-    };
-    
-    var pageProperty = dispatcher_stream.filter(function(event) {
-      return event.action === 'page';
-    }).map(function(event) {
-      return event.options.page;
-    }).toProperty(function() {
+    var pageProperty = streams.page_stream.toProperty(function() {
     	return 1;
     });
-    
-    var showIdSelectedStream = dispatcher_stream.filter(function(event) {
-      return "show" === event.action;
-    }).map(function(event) {
-      return event.options.row.id;
+    var selectedProperty = streams.selected_stream.toProperty(function() {
+    	return null;
     });
     
-    dispatcher_stream.onValue(function(options) {
-    	fsm.do_transition(options.action);
-    });
-    
-    var idSelectedStream = showIdSelectedStream.toProperty(function() {
-      return null;
-    });
-    
-    var responseSlectedStream = showIdSelectedStream.flatMapLatest(function(id) {
-      return Kefir.fromPromise( sb.services.get(
-          "users", 'show', {id: id}
-        )
-      );
-    });
-    
-    var selectedProperty = responseSlectedStream.map(function(data) {
-      return data.data;
-    }).toProperty(function() {
-      return null;
-    });
-    
-    var clearSelected = function() {
-      selectedProperty.push(null);
-    };
-    
-    var responseStream = pageProperty.flatMapLatest(function(page) {
-      showSpinner();
-      return Kefir.fromPromise( sb.services.get(
-          "users", 'index', {page: page, per_page: 10}
-        ).then(function(data) {
-          hideSpinner();
-          return data;
-        })
-      );
-    });
-  
-    var recordsProperty = responseStream.map(function(data) {
-      return data.data;
-    }).toProperty(function() {
+    var recordsProperty = streams.records_stream.toProperty(function() {
       return [];
     });
   
-    var totalProperty = responseStream.map(function(data) {
-      return data.total;
-    }).toProperty(function() {
+    var totalProperty = streams.total_stream.toProperty(function() {
       return 0;
     });
     
@@ -164,6 +203,11 @@ AdminJS.modules.users.Model = (function() {
         return parseInt(total/per_page,10)+1;
       }
     );
+
+    var spinnerProperty = streams.spinner_stream.toProperty(function() {
+    	return false;
+    });
+    
     var stream = Kefir.combine([
         pageProperty, 
         perPageProperty,
@@ -189,7 +233,7 @@ AdminJS.modules.users.Model = (function() {
     );
 
     var initialize = function(options) {
-      return fsm.do_transition('init');
+      return streams.initialize();
     };
   
     return {
